eb4b6fe8f5d47337f8e5662d6b0652ef6457f996
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2022-02-24 20:48:15 +0500

    Updated code & minor fixes

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ const getData = async () => {
     const wordArray = words.split("\n");
     const randomIndex = Math.floor(Math.random() * wordArray.length);
     word = wordArray[randomIndex].trim();
-    console.log(word);
     placeholder(word);
 }
 
